8dcc2bc7a8d6edb63bee5b680bdf3625ee0511ec
llemcke <122227030+llemcke@users.noreply.github.com>
2023-08-10 12:14:32 -0400

    Delete Old files

--- Weather/weather.py
+++
@@ -1,9 +0,0 @@
-
-class Weather:
-    def __init__(self, location:str, temperature:int, snow:int, rain:int,wind:int):
-        self.location=location
-        self.rain=rain
-        self.snow=snow
-        self.wind=wind
-        self.temperature=temperature
-        return
